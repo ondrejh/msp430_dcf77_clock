6c8691bd0e8c292b1bd03f82870fc661863c0920
ondrej <ondrejh.ck@gmail.com>
2012-10-02 11:01:45 +0200

    32kHz Xtal timer basics

---
+++ main.c
@@ -0,0 +1,82 @@
+//******************************************************************************
+// DCF77 home automation clock
+//
+// author: Ondrej Hejda
+// date:   2.10.2012
+//
+// resources:
+//
+//  Using ACLK and the 32kHz Crystal .. thanks to:
+//   "http://www.msp430launchpad.com/2012/03/using-aclk-and-32khz-crystal.html"
+//
+// hardware: MSP430G2553 (launchpad)
+//
+//                MSP4302553
+//             -----------------
+//         /|\|              XIN|----  -----------
+//          | |                 |     | 32.768kHz |---
+//          --|RST          XOUT|----  -----------    |
+//            |                 |                    ---
+//            |                 |
+//            |           P1.1,2|--> UART (debug output 9.6kBaud)
+//            |                 |
+//            |             P1.0|--> RED LED (active high)
+//            |             P1.6|--> GREEN LED (active high)
+//            |                 |
+//
+//******************************************************************************
+
+// include section
+#include <msp430g2553.h>
+
+// board (leds, button)
+#define LED_INIT() {P1DIR|=0x41;P1OUT&=~0x41;}
+#define LED_RED_ON() {P1OUT|=0x01;}
+#define LED_RED_OFF() {P1OUT&=~0x01;}
+#define LED_RED_SWAP() {P1OUT^=0x01;}
+#define LED_GREEN_ON() {P1OUT|=0x40;}
+#define LED_GREEN_OFF() {P1OUT&=~0x40;}
+#define LED_GREEN_SWAP() {P1OUT^=0x40;}
+
+// init rtc timer (32kHz Xtal)
+void rtc_timer_init(void)
+{
+	CCTL0 = CCIE; // CCR0 interrupt enabled
+	CCR0 = 512;	  // f = 32768 / 8(ID_3) / 512(CCR0) = 8Hz
+	TACTL = TASSEL_1 + ID_3 + MC_1; // ACLK, /8, upmode
+}
+
+// leds and dco init
+void board_init(void)
+{
+	// oscillator
+	BCSCTL1 = CALBC1_1MHZ;		// Set DCO
+	DCOCTL = CALDCO_1MHZ;
+	/*BCSCTL1 = CALBC1_16MHZ;		// Set DCO
+	DCOCTL = CALDCO_16MHZ;*/
+
+	LED_INIT(); // leds
+}
+
+// main program body
+int main(void)
+{
+	WDTCTL = WDTPW + WDTHOLD;	// Stop WDT
+
+	board_init(); // init dco and leds
+	rtc_timer_init(); // init 32kHz timer
+
+	while(1)
+	{
+        __bis_SR_register(CPUOFF + GIE); // enter sleep mode (leave on timer interrupt)
+	}
+
+	return -1;
+}
+
+// Timer A0 interrupt service routine
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
+{
+	LED_GREEN_SWAP();
+}
